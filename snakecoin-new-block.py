2e28c5da4a15fa280e6fc90560e61f39fcb651d4
Saurabh S. Chaturvedi <saurabh.chaturvedi63@gmail.com>
2017-07-18 18:16:34 +0530

    Add function to create new block

---
+++ snakecoin-new-block.py
@@ -0,0 +1,5 @@
+def next_block(last_block):
+    this_index = last_block.index + 1
+    this_timestamp = datetime.datetime.now()
+    this_data = "Hey! I'm block " + str(this_index)
+    return Block(this_index, this_timestamp, this_data, last_block.hash)
