308cc4b5809efb06ea98fe17f7b8a3b630cbbb4c
Saurabh S. Chaturvedi <saurabh.chaturvedi63@gmail.com>
2017-07-18 17:58:44 +0530

    Create block structure

---
+++ snakecoin-block.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import hashlib as hasher
+
+class Block():
+    def __init__(self, index, timestamp, data, previous_hash):
+        self.index = index
+        self.timestamp = timestamp
+        self.data = data
+        self.previous_hash = previous_hash
+        self.hash = self.hash_block()
+
+    def  hash_block(self):
+        sha = hasher.sha_256()
+        sha.update(str(self.index)
+            + str(self.timestamp)
+            + str(self.data)
+            + str(self.previous_hash)
+        )
+        return sha.hexdigest()
